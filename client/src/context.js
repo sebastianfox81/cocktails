696e6e44d4200ae889bb1f2718e4a02740ff68c4
Sebastian <sebastianfox81@gmail.com>
2022-02-01 13:27:51 -0500

    Implemented fetchCocktails function in context.js

--- client/src/context.js
+++ client/src/context.js
@@ -7,8 +7,45 @@ const AppContext = React.createContext()
 const AppProvider = ({ children }) => {
 
   const [ loading, setLoading ] = useState(true);
-  const [ searchTerm, setSearchTerm ] = useState('a');
+  const [ searchTerm, setSearchTerm ] = useState('margarita');
   const [ cocktails, setCocktails ] = useState([]);
+
+  const fetchDrinks = async () => {
+    try {
+      setLoading(true)
+      const res = await fetch(`${url}${searchTerm}`);
+      const data = await res.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            img: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass
+          }
+          setCocktails(newCocktails)
+        })
+      } else {
+        setCocktails([])
+      }
+      setLoading(false)
+      console.log(drinks)
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm])
+
+
+
 
   return <AppContext.Provider
   value={
