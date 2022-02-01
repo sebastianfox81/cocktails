04a6156e53534bab7b404e36c0d17da911bf09c5
Sebastian <sebastianfox81@gmail.com>
2022-02-01 12:47:35 -0500

    Basic setup for about & error pages

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque repudiandae delectus vitae sint maxime dolorem non dignissimos. Reprehenderit nemo possimus eveniet nisi quos nesciunt quidem hic ratione, odit molestias porro.</p>
+    </section>
   )
 }
 
